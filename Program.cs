f8e0fd6f8e06b0f8ca0943267edfc69222bd8f3d
Roman20282 <romanpodlesnij1982@gmail.com>
2021-03-18 10:12:47 +0200

    Stack

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            
+            string ch;
+            bool Eexit = false;
+            while (!Eexit)
+            {
+                Console.Clear();
+                Console.WriteLine("****************Program using class Stack().**********************");
+                Console.WriteLine("==================================================================");
+                Console.WriteLine("                 1) Push objects in Stack                         ");
+                Console.WriteLine("                 2) Pop objects from Stack                        ");
+                Console.WriteLine("                 3) Clear Stack                                   ");
+                Console.WriteLine("                 4) End program.                                  ");
+                Console.WriteLine("==================================================================");
+                Console.Write(" Make your choice    :   "); ch = Console.ReadLine();
+                if (ch == "4") Eexit = true;
+                else
+                {
+                    if (ch == "1")
+                    {
+                        Console.WriteLine("Press Esc to exit.");
+                        ConsoleKeyInfo input;
+                        do
+                        {
+                           input = Console.ReadKey(true);
+                           if (input.Key != ConsoleKey.Escape)
+                            {
+                                Console.WriteLine(input.Key.ToString());
+                                stack.Push(input.Key.ToString());
+                            }
+
+                        } while (input.Key != ConsoleKey.Escape);
+                        Console.WriteLine("Items in Stack.");
+                        Console.WriteLine("Press any key...");
+                        Console.ReadLine();
+
+                    }
+                    if ((ch == "2") & (stack.ElementCount > 0))
+                    {
+                        Console.WriteLine("Items from Stack.");
+                        do
+                        {
+                            Console.WriteLine(stack.Pop());
+                        }
+                        while (stack.ElementCount > 0);
+                        Console.WriteLine("Press any key...");
+                        Console.ReadLine();
+                    }
+                    else if ((ch == "2") & (stack.ElementCount == 0)){ 
+                            Console.WriteLine("Stack empty");
+                            Console.WriteLine("Press any key...");
+                            Console.ReadLine(); 
+                    }
+                    if (ch == "3") { stack.Clear(); Console.WriteLine("Stack empty. Press any key..."); Console.ReadLine(); }
+                    }
+                
+
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    public class Stack
+    {
+        object x;
+        ArrayList list = new ArrayList();
+        public int ElementCount { get; private set; }
+
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                list.Add(obj);
+                ElementCount++;
+            }
+            else
+            {
+                Console.WriteLine("InvalidOperationException: object can't be null. Element not added. Error:  -1 ");
+            }
+                
+        }
+        public object Pop()
+        {
+            if (ElementCount >= 1)                     
+            {
+                x = list[ElementCount-1];
+                list[ElementCount - 1] = null;
+                ElementCount--;
+                return x;
+            }
+            else
+            {
+                Console.WriteLine("InvalidOperationException: Stack empty.  Error: ");
+                return -1;
+            }
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            ElementCount = 0;
+
+        }
+    }
+}
